a43a052e0a34edfca8dee93968294e667f826a3b
Nicolai Kopka <nicolai.kopka@aol.de>
2022-05-19 14:54:58 +0200

    revision Array Test

--- src/test/java/ArrayChallengeTest.java
+++ src/test/java/ArrayChallengeTest.java
@@ -4,7 +4,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayChallengeTest {
-    
+
+    @Test
+    void shouldReturnEmptyArrayIfInitial0() {
+        String[] expectedArray = new String[0];
+        String[] actualArray = ArrayChallenge.createStudentArray(0);
+        Assertions.assertArrayEquals(expectedArray, actualArray);
+    }
     @Test
     void shouldReturnCorrectStringsFromArray() {
         String[] testArray = {"Student 1", "Student 2", "Student 3", "Student 4", "Student 5"};
